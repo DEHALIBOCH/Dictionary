8004e10f0606306ce18e85b186a3bf6aa97f43c8
Marat <almabekov.marat@gmail.com>
2023-07-20 01:32:38 +0600

    Реализация Репозитория

---
+++ app/src/main/java/com/example/dictionary/data/repository/WordInfoRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.example.dictionary.data.repository
+
+import com.example.dictionary.data.local.WordInfoDAO
+import com.example.dictionary.data.remote.DictionaryApi
+import com.example.dictionary.domain.model.WordInfo
+import com.example.dictionary.domain.repository.WordInfoRepository
+import com.example.dictionary.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class WordInfoRepositoryImpl(
+    private val api: DictionaryApi,
+    private val dao: WordInfoDAO,
+) : WordInfoRepository {
+
+    override fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>> = flow {
+        emit(Resource.Loading())
+
+        val wordInfos = dao.getWordInfos(word).map { it.toWordInfo() }
+        emit(Resource.Loading(data = wordInfos))
+
+        try {
+            val remoteWordInfos = api.getWordInfo(word)
+            dao.deleteWordInfos(remoteWordInfos.map { it.word })
+            dao.insertWordInfos(remoteWordInfos.map { it.toWordInfoEntity() })
+        } catch (e: HttpException) {
+
+        } catch (e: IOException) {
+
+        }
+    }
+}
