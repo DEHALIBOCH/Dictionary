08497993c5b8a5a4a68d1c4b3cbc7d89d4b5662d
Marat <almabekov.marat@gmail.com>
2023-07-20 00:04:55 +0600

    Предварительная подготовка

--- app/src/main/java/com/example/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.viewbinding.library.activity.viewBinding
+import com.example.dictionary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
