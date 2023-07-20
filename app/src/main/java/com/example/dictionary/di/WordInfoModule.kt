f0cdc6eb3500d6c879ea1eda00dec05d3e8343ee
Marat <almabekov.marat@gmail.com>
2023-07-20 23:46:27 +0600

    Фикс TypeConverter в Room

---
+++ app/src/main/java/com/example/dictionary/di/WordInfoModule.kt
@@ -0,0 +1,55 @@
+package com.example.dictionary.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.dictionary.data.local.Converters
+import com.example.dictionary.data.local.WordInfoDAO
+import com.example.dictionary.data.local.WordInfoDatabase
+import com.example.dictionary.data.remote.DictionaryApi
+import com.example.dictionary.data.repository.WordInfoRepositoryImpl
+import com.example.dictionary.data.util.GsonParser
+import com.example.dictionary.domain.repository.WordInfoRepository
+import com.example.dictionary.domain.use_case.GetWordInfo
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WordInfoModule {
+
+    @Provides
+    @Singleton
+    fun provideGetWordInfoUseCase(repository: WordInfoRepository): GetWordInfo {
+        return GetWordInfo(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordInfoRepository(api: DictionaryApi, db: WordInfoDatabase): WordInfoRepository {
+        return WordInfoRepositoryImpl(api, db.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordInfoDatabase(app: Application): WordInfoDatabase {
+        return Room.databaseBuilder(app, WordInfoDatabase::class.java, "word_db")
+            .addTypeConverter(Converters(GsonParser(Gson())))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryApi(): DictionaryApi {
+        return Retrofit.Builder()
+            .baseUrl(DictionaryApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DictionaryApi::class.java)
+    }
+}
