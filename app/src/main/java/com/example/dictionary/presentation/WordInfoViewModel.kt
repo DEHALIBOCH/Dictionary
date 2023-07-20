14b03aecb91f0b3ac79d4120d0f73833305e9a06
Marat <almabekov.marat@gmail.com>
2023-07-20 23:48:01 +0600

    Добавление функионала в ViewModel

--- app/src/main/java/com/example/dictionary/presentation/WordInfoViewModel.kt
+++ app/src/main/java/com/example/dictionary/presentation/WordInfoViewModel.kt
@@ -9,8 +9,6 @@ import com.example.dictionary.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -21,21 +19,22 @@ class WordInfoViewModel @Inject constructor(
     private val getWordInfo: GetWordInfo
 ) : ViewModel() {
 
-    private val _searchQuery = MutableLiveData<String>()
-    val searchQuery: LiveData<String>
-        get() = _searchQuery
+//    private val _searchQuery = MutableLiveData<String>()
+//    val searchQuery: LiveData<String>
+//        get() = _searchQuery
 
-    private val _wordInfoState = MutableLiveData<WordInfoState>()
+    private val _wordInfoState = MutableLiveData<WordInfoState>(WordInfoState())
     val wordInfoState: LiveData<WordInfoState>
         get() = _wordInfoState
 
-    private val _eventFlow = MutableSharedFlow<UIEvent>()
-    private val eventFlow = _eventFlow.asSharedFlow()
+    private val _uiEvent = MutableLiveData<UIEvent>()
+    val uiEvent: LiveData<UIEvent>
+        get() = _uiEvent
 
     private var searchJob: Job? = null
 
     fun onSearch(query: String) {
-        _searchQuery.value = query
+//        _searchQuery.value = query
         searchJob?.cancel()
         searchJob = viewModelScope.launch {
             delay(500L)
@@ -47,15 +46,15 @@ class WordInfoViewModel @Inject constructor(
                             isLoading = false
                         )
                     }
+
                     is Resource.Error -> {
                         _wordInfoState.value = wordInfoState.value?.copy(
                             wordInfoItems = result.data ?: emptyList(),
                             isLoading = false
                         )
-                        _eventFlow.emit(UIEvent.ShowSnackbar(
-                            result.message ?: "Unknown Error"
-                        ))
+                        _uiEvent.value = UIEvent.ShowSnackbar(result.message ?: "Unknown Error")
                     }
+
                     is Resource.Loading -> {
                         _wordInfoState.value = wordInfoState.value?.copy(
                             wordInfoItems = result.data ?: emptyList(),
